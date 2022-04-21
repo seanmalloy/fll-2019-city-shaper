6c692a6d259a699928723a055e974ace989dba49
Sean Malloy <spinelli85@gmail.com>
2022-04-20 23:20:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ PORT_D = "D" # right motor
 #PORT_F - big motor
 
 hub = PrimeHub()
+motor_pair = MotorPair(PORT_C, PORT_D) 
 right_motor = Motor(PORT_D)
 left_motor = Motor(PORT_C)
 
-def move_forward(speed: int):
-    right_motor.start(speed)
-    left_motor.start(-speed)
+def move_forward(distance: float, speed: int):
+    motor_pair.move(distance, "in", 0, speed) # TODO: don't hard code these parameters
 
-def move_backward(speed: int):
-    right_motor.start(-speed)
-    left_motor.start(speed)
+def move_backward(distance: float, speed: int):
+    #right_motor.start(-speed)
+    #left_motor.start(speed)
+    motor_pair.move(distance, "in", 0, -speed) # TODO: don't hard code these parameters
 
 def turn_right(speed: int, degrees: int):
     stop()
@@ -45,9 +46,17 @@ def stop():
     left_motor.stop()
 
 def main():
+    motor_percent_speed = 50
     while True:
         if hub.right_button.was_pressed():
-            turn_left(50, 179)
+            # Push red house into red circle and return
+            move_forward(25, motor_percent_speed)
+            move_backward(25, motor_percent_speed)
+        if hub.left_button.was_pressed():
+            # Climb the bridge
+            move_forward(41, motor_percent_speed)
+            turn_left(motor_percent_speed, 115)
+            move_forward(28, motor_percent_speed)
             
 
 main()
